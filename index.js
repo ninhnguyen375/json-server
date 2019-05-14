a1c9382fa1a33030039decb38e7f96fa3a15590d
Ninh <ninhnguyen375@gmail.com>
2019-05-14 08:37:17 +0700

    setup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-app.listen(port, () => console.log('Starting...'));
+server.listen(port, () => console.log('Starting...'));
